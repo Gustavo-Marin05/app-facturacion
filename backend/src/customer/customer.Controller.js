b51de982859f4e87ecce7ca1c1497a4c717702c2
Juan55XXD <vasquezmarcelito0@gmail.com>
2025-03-23 11:32:30 -0900

    Agregada createCustomerv1

--- backend/src/customer/customer.Controller.js
+++ backend/src/customer/customer.Controller.js
@@ -1,7 +1,18 @@
 
-import { deleteCustomer, getaCustomer, getAllCustomer, updateCustomerById } from "./customerService.js";
+import { createCustomer,deleteCustomer, getaCustomer, getAllCustomer, updateCustomerById } from "./customerService.js";
 
-
+// Crear un cliente
+export const createCustomerController = async (req, res) => {
+  try {
+    const newCustomer = await createCustomer(req.user.id, req.body); // req.user.id viene del token
+    if (newCustomer.error) {
+      return res.status(400).json({ error: newCustomer.error });
+    }
+    res.status(201).json(newCustomer);
+  } catch (error) {
+    res.status(500).json({ error: "Error al crear el cliente" });
+  }
+};
 
 
 //obtener todos los clientes
